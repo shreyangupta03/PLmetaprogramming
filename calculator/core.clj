47ef7104347cf4d9319f7d650d6ffd79771d21ce
Pannaga-Bhat <131283821+Pannaga-Bhat@users.noreply.github.com>
2024-04-22 15:23:30 +0530

    Updated calculator/core.clj

--- calculator/core.clj
+++ calculator/core.clj
@@ -4,10 +4,10 @@
 (defmacro generate-arithmetic-function [op-symbol]
   `(fn [a# b#] (~op-symbol (double a#) (double b#))))
 
-(defn factorial [n]
+(defn factorial [n m]
   (if (<= n 1)
     1
-    (* n (factorial (dec n)))))
+    (* n (factorial (dec n) m))))
 
 (defn log-function [f]
   (fn [& args]
@@ -15,34 +15,13 @@
       (println (str "Calling function: " (str f) ", Arguments: " args ", Result: " result))
       result)))
 
-(defn add-with-logging [a b]
-  ((log-function +) a b))
-
-(defn sub-with-logging [a b]
-  ((log-function -) a b))
-
-(defn mul-with-logging [a b]
-  ((log-function *) a b))
-
-(defn div-with-logging [a b]
-  ((log-function /) a b))
-
-(defn exp-with-logging [a b]
-  ((log-function (fn [x y] (Math/pow x y))) a b))
-
-(defn mod-with-logging [a b]
-  ((log-function mod) a b))
-
-(defn fact-with-logging [a b]
-  ((log-function factorial) a))
-
-(def add (generate-arithmetic-function add-with-logging))
-(def sub (generate-arithmetic-function sub-with-logging))
-(def mul (generate-arithmetic-function mul-with-logging))
-(def div (generate-arithmetic-function div-with-logging))
-(def exp (generate-arithmetic-function exp-with-logging))
-(def modu (generate-arithmetic-function mod-with-logging))
-(def fact (generate-arithmetic-function fact-with-logging))
+(def add (generate-arithmetic-function (log-function +)))
+(def sub (generate-arithmetic-function (log-function -)))
+(def mul (generate-arithmetic-function (log-function *)))
+(def div (generate-arithmetic-function (log-function /)))
+(def exp (generate-arithmetic-function (log-function (fn [x y] (Math/pow x y)))))
+(def modu (generate-arithmetic-function (log-function mod)))
+(def fact (generate-arithmetic-function (log-function factorial)))
 
 (defn perform-operation [operation a b]
   (let [op-fn (get {:add add :sub sub :mul mul :div div :exp exp :mod modu :fact fact} operation)]
